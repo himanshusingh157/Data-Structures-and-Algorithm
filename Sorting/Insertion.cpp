e7e36076102991168db45f1f6fe538c29f087507
Himanshu Singh <68109107+himanshusingh157@users.noreply.github.com>
2023-05-27 15:44:22 +0530

    Create Insertion.cpp

---
+++ Sorting/Insertion.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include <cstdlib>
+
+int main()
+{
+	int n =50;
+	int A[n];
+	printf("Unsorted Array: \n");
+	for(int i=0; i<n;i++)
+	{
+		A[i] = rand();
+		printf("%d, ",A[i]);
+	}
+	
+	for(int i=1;i<n;i++)
+	{
+		int key =A[i];
+		for(int j =0;j<i;j++)
+		{
+			if(A[j]>A[i])
+			{
+				for(int k =i;k>j;k--)
+				{
+					A[k]=A[k-1];
+				}
+				A[j]=key;
+			}
+		}
+	}
+	printf("\nSorted Array: \n");
+	for(int i=0;i<n-1;i++)
+	{
+		printf("%d, ",A[i]);
+	}
+	printf("%d",A[n-1]);
+}
